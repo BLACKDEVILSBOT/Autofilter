88c0ee31af5a16b01ea2989347a12fd2b8137cc3
Hsjsjeje7 <90348733+Hsjsjeje7@users.noreply.github.com>
2021-09-16 21:04:17 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -5,49 +5,49 @@
 class Translation(object):
     
     START_TEXT = """<b>ഹായ് {}!!</b>
-<b>ഞാൻ ഒരു ഗ്രൂപ്പ് മാനേജർ ബോട്ട് ആണ് പക്ഷെ വെറുതെ സമയം കളയണ്ട എന്നെ മൂവി ഹട്ട് ഗ്രൂപ്പിൽ മാത്രമേ ഉപയോഗിക്കാൻ കഴിയൂ എന്റെ വിധി എൻറെ അഡ്മിന്റെ തീരുമാനങ്ങളാണ്.</b>
-<b></b>
+<b>ഞാൻ ഒരു ഗ്രൂപ്പ് മാനേജർ ബോട്ട് ആണ് പക്ഷെ വെറുതെ സമയം കളയണ്ട എന്നെ മൂവി ഹട്ട് ഗ്രൂപ്പിൽ മാത്രമേ ഉപയോഗിക്കാൻ കഴിയൂ.</b>
+<b>© @THOMAS_MOVIE_HUT</b>
 <b></b>
 <b></b>
 <b></b>""" 
     HELP_TEXT = """
-<b>😉How To Use Me!😉?</b>
+<b>Don't Use Me😅!?</b>
 
 <i>
--> Add Me To Any Group And Make Me Admin
--> Add Me To Your Desired Channel
+->
+->
 </i>
 
-<b>Bot Commands (Works Only In Groups) :</b>
+<b>ഒന്ന് പോടാ (അവൻ help ചോദിച്ചു വന്നിരികുന്നു😒😏) :</b>
 
     -> <code>/add chat_id</code>
-                OR                  - <b>To Connect A Group With A Channel (Bot Should Be Admin With Full Previlages In Both Group And Channel)</b>
+                OR                  - <b></b>
      <code>/add @Username</code>
      
     -> <code>/del chat_id</code>
-                OR                  - <b>To disconnect A Group With A Channel</b>
+                OR                  - <b></b>
      <code>/del @Username</code>
      
-    -> <code>/delall</code>  - <b>This Command Will Disconnect All Connected Channel With The Group And Deletes All Its File From DB</b>
+    -> <code>/delall</code>  - <b></b>
     
-    -> <code>/settings</code> -  <b>This Command Will Display You A Settings Pannel Instance Which Can Be Used To Tweek Bot's Settings Accordingly</b>
+    -> <code>/settings</code> -  <b></b>
 
-            -> <code>Channel</code> - <b>Button Will Show You All The Connected Chats With The Group And Will Show Buttons Correspnding To There Order For Furthur Controls</b>
+            -> <code>Channel</code> - <b></b>
             
-            -> <code>Filter Types</code> - <b>Button Will Show You The 3 Filter Option Available In Bot... Pressing Each Buttons Will Either Enable or Disable Them And This Will Take Into Action As Soon As You Use Them Without The Need Of A Restart</b>
+            -> <code>Filter Types</code> - <b></b>
 
-            -> <code>Configure</code> - <b>Button Will Helps You To Change No. of Pages/ Buttons Per Page/ Total Result Without Acutally Editing The Repo... Also It Provide Option To Enable/Disable For Showing Invite Link In Each Results</b>
+            -> <code>Configure</code> - <b></b>
             
-            -> <code>Status</code> - <b>Button Will Shows The Stats Of Your Channel</b>
+            -> <code>Status</code> - <b></b>
             
-<b><a href="https://t.me/@THOMAS_MOVIE_HUT">© THOMAS_MOVIE_HUT</a></b>
+<b><a href="https://t.me/THOMAS_MOVIE_HUT">© THOMAS_MOVIE_HUT</a></b>
 """
     
     ABOUT_TEXT = """<b>➥ 😎Name</b> : <b>Pro Auto Filter Bot</b>
 
-<b>>➥😈Master</b> : <b><a href="https://t.me/THOMAS_MOVIE_HUT">👤THOMAS_MOVIE_HUT</a></b>
+<b>>➥🧑Master</b> : <b><a href="https://t.me/THOMAS_MOVIE_HUT">👤THOMAS_MOVIE_HUT</a></b>
 
-<b>➥ 🔷Editor</b> : <b><a href="https://t.me/THOMAS_MOVIE_HUT">👤THOMAS_MOVIE_HUT</a></b>
+<b>➥ 🛠️Editor</b> : <b><a href="https://t.me/THOMAS_MOVIE_HUT">👤THOMAS_MOVIE_HUT</a></b>
 
 <b>➥ 🔥Language</b> : <b>Python3<b>
 
